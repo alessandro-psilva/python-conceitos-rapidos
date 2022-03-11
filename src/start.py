e91a04f1dea7f8c0d706be404f0e2a851a3a4a7a
ALESSANDRO SILVA <alessandro-psilva@oulook.com>
2022-03-11 20:10:36 -0300

    add: settings project

---
+++ src/start.py
@@ -0,0 +1,13 @@
+from typing import Union
+
+
+def agregation(
+    element_1: Union[int, float], element_2: Union[int, float]
+) -> Union[int, float]:
+    """Agregation of two elements
+
+    Args:
+        element_1 (int): element one
+        element_2 (int): element two
+    """
+    return element_1 + element_2
---
+++ src/test_start.py
@@ -0,0 +1,6 @@
+from .start import agregation
+
+
+def test_agregation():
+    """Test agregation"""
+    assert 11 == agregation(element_1=10, element_2=1)
